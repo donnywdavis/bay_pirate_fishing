6cdf1121aceea741c9b780031ff501b1b2f28157
Donny Davis <donnywdavis@icloud.com>
2015-07-08 14:08:12 -0400

    Add model and migrations for the schedules

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Schedule(models.Model):
+    event = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    start_date = models.DateField(blank=True)
+    end_date = models.DateField(blank=True)
+    rank = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return self.event
