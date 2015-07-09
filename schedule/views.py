520351590f28f74669e22b96d97711343e6cac86
Donny Davis <donnywdavis@icloud.com>
2015-07-08 21:44:00 -0400

    Add view and template for the schedule

--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Event
 
-# Create your views here.
+
+def index(request):
+    events = Event.objects.all()
+    context = {
+        'events': events
+    }
+    return render(request, 'schedule/index.html', context)
