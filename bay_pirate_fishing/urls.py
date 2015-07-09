b5f7251f9528b9423dedd487432027008fd410af
Donny Davis <donnywdavis@icloud.com>
2015-07-08 21:40:47 -0400

    Add route for schedule and context data for the home page

--- bay_pirate_fishing/urls.py
+++ bay_pirate_fishing/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^about/', 'bay_pirate_fishing.views.about', name='about'),
+    url(r'^schedule/', include('schedule.urls', namespace='schedule')),
     url(r'^$', 'bay_pirate_fishing.views.home', name='home'),
 ]
